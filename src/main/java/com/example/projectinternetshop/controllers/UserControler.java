d3ade9a066827cf5aa50d66294f0d700dfe689ef
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-12-12 14:42:25 +0300

    update style info

--- src/main/java/com/example/projectinternetshop/controllers/UserControler.java
+++ src/main/java/com/example/projectinternetshop/controllers/UserControler.java
@@ -48,6 +48,7 @@ public class UserControler {
     @GetMapping("user/info/{id}")
     public String infoProduct(@PathVariable("id") int id, Model model) {
         model.addAttribute("product", productService.getProductById(id));
+        model.addAttribute("person", personDetails());
         return "user/infoProduct";
     }
 
