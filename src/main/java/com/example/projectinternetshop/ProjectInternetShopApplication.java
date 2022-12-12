0d671ce26faef41ab7e8c6e1c99778f0d28cbc41
AlexeyKonstantinov78 <alex_kav@mail.ru>
2022-12-12 10:46:33 +0300

    init

---
+++ src/main/java/com/example/projectinternetshop/ProjectInternetShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectinternetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectInternetShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectInternetShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectinternetshop/ProjectInternetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectinternetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectInternetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
